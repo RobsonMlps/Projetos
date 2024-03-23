515f334b02ac97a6047a96682b2429ef7bfed647
RobsonMlps <105452402+RobsonMlps@users.noreply.github.com>
2024-03-22 21:46:51 -0300

    Coração em python

---
+++ coração.py
@@ -0,0 +1,12 @@
+from turtle import begin_fill, circle, color, end_fill, forward, left, pensize, right
+
+color("red")
+begin_fill()
+pensize(10)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200) 
+forward(133)
+end_fill()
